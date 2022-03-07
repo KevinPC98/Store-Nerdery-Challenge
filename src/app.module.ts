8c7926bed985a51e3e1268fa455a4f34cd5e312e
unknown <kevin.paredescaman20@gmail.com>
2022-03-07 14:45:49 -0500

    feat: Project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { LikeService } from './like/like.service';
+import { ProductController } from './product/product.controller';
+import { LikeController } from './like/like.controller';
+import { ProductService } from './product/product.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController, ProductController, LikeController],
+  providers: [AppService, AuthService, LikeService, ProductService],
 })
 export class AppModule {}
