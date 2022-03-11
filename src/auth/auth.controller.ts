f292a65f653ebcd0504c268c4d70a1f61a532b2e
unknown <kevin.paredescaman20@gmail.com>
2022-03-11 09:35:50 -0500

    feat: authentication with roles

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,28 @@
 import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Role } from '../utils/enums';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/request/auth-credentials.dto';
 import { TokenDto } from './dto/response/token.dto';
+import { Roles } from './role/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  //@Roles(Role.client, Role.manager)
   @Post('/signin')
   singin(@Body() authCredentialsDto: AuthCredentialsDto): Promise<TokenDto> {
-    console.log(authCredentialsDto);
     return this.authService.signIn(authCredentialsDto);
   }
+
+  //@Roles(Role.client, Role.manager)
   @Post('/signup')
   singup(@Body() createUserDto: CreateUserDto): Promise<TokenDto> {
     return this.authService.signUp(createUserDto);
   }
 
+  //@Roles(Role.client, Role.manager)
   @Get('/signout')
   singout(@Req() req): Promise<void> {
     const token = req.headers.authorization.replace('Bearer ', '');
---
+++ src/utils/enums.ts
@@ -0,0 +1,4 @@
+export enum Role {
+  client = 'C',
+  manager = 'M',
+}
