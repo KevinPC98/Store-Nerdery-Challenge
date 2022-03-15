cce4b0e1e3ab5b3c59321605335614983b5af7bc
unknown <kevin.paredescaman20@gmail.com>
2022-03-14 23:56:34 -0500

    adding manager user

--- prisma/migrations/20220311051920_default_value_true_of_visible_from_product/migration.sql
+++ prisma/migrations/20220311051920_default_value_true_of_visible_from_product/migration.sql
@@ -1,2 +1,19 @@
 -- AlterTable
-ALTER TABLE "products" ALTER COLUMN "visible" SET DEFAULT true;
+
+ALTER TABLE "products"
+ALTER COLUMN "visible"
+SET DEFAULT true;
+
+
+INSERT INTO "users" ("id",
+                     "name",
+                     "email",
+                     "password",
+                     "created_at",
+                     "role")
+VALUES ('57be8001-27a5-409d-bc4f-ff5630ee88bb',
+        'kevin',
+        'kevin@admin.com',
+        '$2a$10$2AFij9VuF2O4jqVK8UErweGZRb4/QO16AXPdPu/uyp6D5ctzYfsU',
+        NOW(),
+        'M')
